693123bbd65c201da3b7c2e5ee5cc2d7b25d7e3a
Marco <marcopaglicci22@gmail.com>
2024-05-21 12:09:22 +0200

    Commit file su git hub

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+}
